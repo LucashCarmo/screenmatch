8517a174c82887eb24cb22cbc036e0e298a4fd61
Lucas Carmo <gamerlucas96@gmail.com>
2023-10-28 20:06:44 -0300

    teste

--- src/br/com/alura/screenmatch/pricipal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/pricipal/PrincipalComListas.java
@@ -25,7 +25,7 @@ public class PrincipalComListas {
         for (Titulo item: lista) {
             System.out.println(item.getNome());
             if(item instanceof Filme filme && filme.getClassificacao() > 2) {
-                System.out.println("Classificação " + filme.getClassificacao());
+                System.out.println("Classificação: " + filme.getClassificacao());
             }
 
         }
