46a04630641e81d8494f03f3afa13e6f608b4ca1
Lucas Carmo <gamerlucas96@gmail.com>
2023-10-29 23:40:11 -0300

    terminado a sessão do curso

--- src/br/com/alura/screenmatch/pricipal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/pricipal/PrincipalComListas.java
@@ -5,6 +5,8 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -29,5 +31,25 @@ public class PrincipalComListas {
             }
 
         }
+
+        ArrayList<String> buscarPorArtista = new ArrayList<>();
+        buscarPorArtista.add("Adam Sandler");
+        buscarPorArtista.add("Paulo");
+        buscarPorArtista.add("Jacqueline");
+        System.out.println(buscarPorArtista);
+
+        Collections.sort(buscarPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscarPorArtista);
+        System.out.println("Lista de titulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano de lançamento");
+        System.out.println(lista );
+
+
+
+
     }
 }
